3bbbe2722113e6cb4b011d8f7b299d0145b283fa
Simon Amsler <simon.amsler@bbzbl-it.ch>
2024-07-05 15:56:09 +0200

    Add 'real' Sample data

--- data/HvAttackData.sql
+++ data/HvAttackData.sql
@@ -1,3 +1,3 @@
 INSERT INTO hvattack_data
 (thlvl, armycomp, description, guide, name)
-VALUES (3, E'Sample Data \<br>Sample Data', 'Sample Data', E'Sample Data \<br> Sample Data', 'Sample Data');
+VALUES (3, E'Barbarians and archers, possibly to add giants or wall breakers', 'Sample Data', E'Sample Data \<br> Sample Data', 'Sample Data');
--- data/HvClanCastleTroopsData.sql
+++ data/HvClanCastleTroopsData.sql
@@ -1,3 +1,3 @@
 insert into hvclan_castle_troops_data (thlvl, description, name, troops)
-values (3,'Sample Data', 'Sample', 'Sample Data \<br> Sample data');
+values (3,'This is best at all lower TH levels, as the baby dragons are quite hard to kill with just archers for example', 'Baby Dragon', '1x Baby Dragon and fillers');
 
